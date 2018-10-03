9e5e413ddfbc85cfc981b1de28fa75199b2e953b
Davyd McColl <davydm@gmail.com>
2018-10-03 15:56:59 +0200

    fix: lint errors

--- src/app/drop-down-toggle.directive.spec.ts
+++ src/app/drop-down-toggle.directive.spec.ts
@@ -1,7 +1,7 @@
-import { DropDownToggleDirective } from './drop-down-toggle.directive';
+import { DropDownToggleDirective } from "./drop-down-toggle.directive";
 
-describe('DropDownToggleDirective', () => {
-  it('should create an instance', () => {
+describe("DropDownToggleDirective", () => {
+  it("should create an instance", () => {
     const directive = new DropDownToggleDirective();
     expect(directive).toBeTruthy();
   });
